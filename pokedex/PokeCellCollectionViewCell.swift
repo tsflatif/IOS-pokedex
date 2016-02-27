7d59bd781a52467e977bb660c9357fc4eb1e4dad
tsflatif <tsflatif@gmail.com>
2016-02-26 19:39:59 -0500

    add pokemon class and storyboard

---
+++ pokedex/PokeCellCollectionViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  PokeCellCollectionViewCell.swift
+//  pokedex
+//
+//  Created by Tauseef Latif on 2016-02-25.
+//  Copyright © 2016 Tauseef Latif. All rights reserved.
+//
+
+import UIKit
+
+class PokeCellCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Tauseef Latif on 2016-02-22.
+//  Copyright © 2016 Tauseef Latif. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
